eab07db0819c9dd91d9228640ace4636b6500299
suelenk <suelenmatosr@gmail.com>
2022-08-07 15:43:51 -0400

    Created the game logic / tracking the tries from user

--- game.py
+++ game.py
@@ -3,5 +3,17 @@ import random
 # Put your code here
 print("Welcome to the Guessing game.")
 name = input("What is your name? ")
-number = int(input("Guess a nunber between 1 - 100: "))
 number_generator = random.randint(1,100)
+tries = 1
+
+while True:
+    user_guess = int(input("Guess a number between 1 - 100: "))
+    if user_guess < number_generator:
+        print("Your guess is too low. Try again.")
+        tries += 1
+    elif user_guess > number_generator:
+        print("your guess is too higly, Try again.")    
+        tries += 1
+    else:
+        print("You are rigth, the number is:", number_generator)
+        break    
