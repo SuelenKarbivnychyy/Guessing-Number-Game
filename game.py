6e94de0067e79f73782541a5e89492b98c11f5aa
suelenk <suelenmatosr@gmail.com>
2022-08-07 15:27:27 -0400

    Asked for user guess/ imported random and generate a random number

--- game.py
+++ game.py
@@ -1,5 +1,7 @@
 """A number-guessing game."""
-
+import random
 # Put your code here
 print("Welcome to the Guessing game.")
-name = input("What is your name?")
+name = input("What is your name? ")
+number = int(input("Guess a nunber between 1 - 100: "))
+number_generator = random.randint(1,100)
