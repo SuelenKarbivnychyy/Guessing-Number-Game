820392428653c67d61ecce8497b79e5b350419cf
suelenk <suelenmatosr@gmail.com>
2022-08-07 16:17:12 -0400

    Add conditional to check if input is number / check if is it in range

--- game.py
+++ game.py
@@ -5,13 +5,22 @@ print("Welcome to the Guessing game.")
 name = input("What is your name? ")
 number_generator = random.randint(1,100)
 tries = 1
+print("The correct number is:", number_generator)
 
 while True:
-    user_guess = int(input("Guess a number between 1 - 100: "))
-    if user_guess < number_generator:
+    user_guess = input("Guess a number between 1 - 100: ")
+    if not user_guess.isdecimal():        
+        print("just positive numbers allow")
+        tries += 1
+        continue
+    user_guess_int = int(user_guess)
+    if user_guess_int < 0 or user_guess_int > 100:
+        print("Your guess is out of range. Enter a number beetween 1 and 100.")
+        tries += 1         
+    elif user_guess_int < number_generator:
         print("Your guess is too low. Try again.")
         tries += 1
-    elif user_guess > number_generator:
+    elif user_guess_int > number_generator:
         print("your guess is too higly, Try again.")    
         tries += 1
     else:
